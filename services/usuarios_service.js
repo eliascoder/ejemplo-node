7e72ece425959c1ac8a7deac8a008553eaa2d6d9
Elias Salinas <asce25@gmail.com>
2021-02-15 17:23:40 -0600

    Implementando await

--- services/usuarios_service.js
+++ services/usuarios_service.js
@@ -1,10 +1,13 @@
-function listar() {
-    return new Promise((resolve) => {
-        resolve({
-            status: 200,
-            response: "Hola mundo desde el service con promises!"
-        })
-    });
+function proceso_interno() {
+    return new Promise(resolve => setTimeout(resolve, 2000));
+}
+
+async function listar() {
+    await proceso_interno();
+    return {
+        status: 200,
+        response: "Hola mundo desde el service con promises!"
+    }
 }
 
 module.exports = {
